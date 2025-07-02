537120e819803b769c5c0c2603ebb9376473132b
Kevin M. Haubrick <32174263+kevinhaubrick@users.noreply.github.com>
2025-07-02 13:30:14 +0000

    Fix failing xmlserializer test when executed on windows

--- src/Tests/MavFiFoundation.SourceGenerators.UnitTests/Serializers/MFFXmlSerializerTests.cs
+++ src/Tests/MavFiFoundation.SourceGenerators.UnitTests/Serializers/MFFXmlSerializerTests.cs
@@ -6,6 +6,6 @@ public class MFFXmlSerializerTests : MFFSerializerTestsBase
     [Fact]
     public void MFFXmlSerializer_Serialize_Deserialize_All_MFFGeneratorInfoModel_Properties ()
     {
-        Serialize_Deserialize_All_MFFGeneratorInfoModel_Properties(new MFFXmlSerializer());
+        Serialize_Deserialize_All_MFFGeneratorInfoModel_Properties(new MFFXmlSerializer(), true);
     }
 }
